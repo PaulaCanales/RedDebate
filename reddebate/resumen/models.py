bbbabb3b1451e59a81335ef4f8967c1cdd82a121
gabrielanicole <gabriela.gonzalezsaez@gmail.com>
2016-10-21 09:24:48 -0300

    elementos para publicar debate

--- reddebate/resumen/models.py
+++ reddebate/resumen/models.py
@@ -1,9 +1,35 @@
 from __future__ import unicode_literals
-
+from django.db.models.fields.related import ForeignKey
+from datetime import *
 from django.db import models
 
 # Create your models here.
-# Create your models here.
 
-#class Prueba(models.Model):
+class Usuario(models.Model):
     #parametros de la tabla. 
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    alias = models.CharField(max_length=30)
+
+    def __unicode__(self): # __unicode__ on Python 2
+		return self.nombre
+
+
+class Debate(models.Model):
+    #parametros de la tabla. 
+    id_debate = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=30)
+    argumento = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.now, blank=True)
+    id_usuario= models.ForeignKey(Usuario)
+
+    def __unicode__(self): # __unicode__ on Python 2
+		return self.titulo
+
+
+
+
+    
+
+
+
--- reddebate/resumen/views.py
+++ reddebate/resumen/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+from django.http import HttpResponse
+from resumen.models import Usuario, Debate
+
+#class PhotoListView(ListView):
+#    model = Photo
+
+
+#class PhotoDetailView(DetailView):
+ #   model = Photo
+
+def index(request):
+	category_list = Debate.objects.all()
+	context = {'object_list': category_list}
+	return render(request, 'index.html', context)
+
+	#titulos= []
+	#argumentos=[]
+#debates = Debate.objects.all()
+#	for i in range(len(debates)):
+#		titulos[i]= debates[i].titulo()
+		#argumentos[i] = debates[i].argumento()
+#	html = "<html><body>It is now %s  .</body></html>" %  titulos
+#	return HttpResponse(html)
