737adc9d4bd53203fca535d1213d24c4d1bf01db
Paula Canales <paulacanales@MacBook-Air-de-APPLE-2.local>
2018-09-12 15:56:09 -0300

    tiempo notificaciones

--- reddebate/resumen/context_processors.py
+++ reddebate/resumen/context_processors.py
@@ -1,10 +1,27 @@
+# coding=utf-8
 from resumen.models import Debate
 from debate.models import Notificacion, Argumento
+from datetime import *
+import pytz
 
 def listado_notificacion(request):
+    utc=pytz.UTC
     notificaciones = Notificacion.objects.all().order_by('-fecha')
     notificacion_usr = []
+    ahora = datetime.now().replace(tzinfo=utc)
     for n in notificaciones:
         if n.id_usuario_id == request.user.id:
-            notificacion_usr.append(n)
+            d = ahora - n.fecha.replace(tzinfo=utc)
+            if d.days == 0:
+                hr = d.seconds//3600
+                min = (d.seconds % 3600) // 60
+                if hr == 0:
+                    if min <= 1: msj = "Ahora"
+                    else: msj = "Hace "+str(min)+" minutos"
+                elif hr == 1: msj = "Hace 1 hora"
+                elif hr > 1: msj = "Hace "+str(hr)+" horas"
+            elif d.days == 1: msj = "Hace "+str(d.days)+" día"
+            elif d.days < 4: msj = "Hace "+str(d.days)+" días"
+            else: msj = n.fecha.strftime('%d/%m/%Y')
+            notificacion_usr.append({'object':n, 'tiempo': msj})
     return {'notificaciones': notificacion_usr}
